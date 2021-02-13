281aafa6d3ba575e05d0d68e867d2324e1d5f83f
Manoj Kumar <manojkchaurasia075@yahoo.com>
2021-02-13 14:27:10 +0530

    Sita Travel

---
+++ SitaTravel/SitaTourAndTravel/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitaTourAndTravel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Star Scaff Shuttering and Fabrication";
+        }
+        public ActionResult Submenu()
+        {
+            return View();
+        }       
+    }
+}
